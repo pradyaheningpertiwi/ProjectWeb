b19e30dc0c4b5ed688da16dbe57f9036b2d4e416
Aliyazalfaputri <112566366+Aliyazalfaputri@users.noreply.github.com>
2024-06-06 14:23:32 +0700

    Add files via upload

---
+++ create-account.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/animations.css">  
+    <link rel="stylesheet" href="css/main.css">  
+    <link rel="stylesheet" href="css/signup.css">
+        
+    <title>Create Account</title>
+    <style>
+        .container{
+            animation: transitionIn-X 0.5s;
+        }
+    </style>
+</head>
+<body>
+<?php
+
+
+session_start();
+
+$_SESSION["user"]="";
+$_SESSION["usertype"]="";
+
+// Set the new timezone
+date_default_timezone_set('Asia/Jakarta');
+$date = date('Y-m-d');
+
+$_SESSION["date"]=$date;
+
+
+include("connection.php");
+
+
+
+
+
+if($_POST){
+
+    $result= $database->query("select * from webuser");
+
+    $fname=$_SESSION['personal']['fname'];
+    $lname=$_SESSION['personal']['lname'];
+    $name=$fname." ".$lname;
+    $address=$_SESSION['personal']['address'];
+    $dob=$_SESSION['personal']['dob'];
+    $email=$_POST['newemail'];
+    $tele=$_POST['tele'];
+    $newpassword=$_POST['newpassword'];
+    $cpassword=$_POST['cpassword'];
+    
+    if ($newpassword==$cpassword){
+        $sqlmain= "select * from webuser where email=?;";
+        $stmt = $database->prepare($sqlmain);
+        $stmt->bind_param("s",$email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if($result->num_rows==1){
+            $error='<label for="promter" class="form-label" style="color:rgb(255, 62, 62);text-align:center;">Alamat Email Sudah Terdaftar.</label>';
+        }else{
+            //TODO
+            $database->query("insert into patient(pemail,pname,ppassword, paddress,pdob,ptel) values('$email','$name','$newpassword','$address','$dob','$tele');");
+            $database->query("insert into webuser values('$email','p')");
+
+            //print_r("insert into patient values($pid,'$email','$fname','$lname','$newpassword','$address','$nic','$dob','$tele');");
+            $_SESSION["user"]=$email;
+            $_SESSION["usertype"]="p";
+            $_SESSION["username"]=$fname;
+
+            header('Location: patient/index.php');
+            $error='<label for="promter" class="form-label" style="color:rgb(255, 62, 62);text-align:center;"></label>';
+        }
+        
+    }else{
+        $error='<label for="promter" class="form-label" style="color:rgb(255, 62, 62);text-align:center;">Terjadi Kesalahan Konfirmasi Kata Sandi! Mohon Konfirmasi Ulang Kata Sandi Anda.</label>';
+    }
+
+
+
+    
+}else{
+    //header('location: signup.php');
+    $error='<label for="promter" class="form-label"></label>';
+}
+
+?>
+
+
+    <center>
+    <div class="container">
+        <table border="0" style="width: 69%;">
+            <tr>
+                <td colspan="2">
+                    <p class="header-text">Mari Bergabung Bersama ByteHealth</p>
+                    <p class="sub-text">Mari Membuat Akun</p>
+                </td>
+            </tr>
+            <tr>
+                <form action="" method="POST" >
+                <td class="label-td" colspan="2">
+                    <label for="newemail" class="form-label">Email: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <input type="email" name="newemail" class="input-text" placeholder="Alamat Email" required>
+                </td>
+                
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <label for="tele" class="form-label">Nomor Telepon: </label>
+                </td>
+            </tr>
+            <tr>
+            <td class="label-td" colspan="2">
+                <input type="tel" name="tele" class="input-text"  placeholder="contoh: 081356935056" pattern="[0-9]{12}" >
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <label for="newpassword" class="form-label">Buat Password Baru: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <input type="password" name="newpassword" class="input-text" placeholder="Password Baru" required>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <label for="cpassword" class="form-label">Konfirmasi Password : </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <input type="password" name="cpassword" class="input-text" placeholder="Konfirmasi Password" required>
+                </td>
+            </tr>
+     
+            <tr>
+                
+                <td colspan="2">
+                    <?php echo $error ?>
+
+                </td>
+            </tr>
+            
+            <tr>
+                <td>
+                    <input type="reset" value="Reset" class="login-btn btn-primary-soft btn" >
+                </td>
+                <td>
+                    <input type="submit" value="Sign Up" class="login-btn btn-primary btn">
+                </td>
+
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <br>
+                    <label for="" class="sub-text" style="font-weight: 280;">Sudah Memiliki Akun&#63; </label>
+                    <a href="login.php" class="hover-link1 non-style-link">Login</a>
+                    <br><br><br>
+                </td>
+            </tr>
+
+                    </form>
+            </tr>
+        </table>
+
+    </div>
+</center>
+</body>
+</html>
